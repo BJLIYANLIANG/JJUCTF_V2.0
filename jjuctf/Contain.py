bc692d57d8822a2543094147376372ce8aa5bfc6
HSM <iobincn@gmail.com>
2020-12-12 21:24:52 +0800

    对Contain类添加开启和结束docker的方法

--- jjuctf/Contain.py
+++ jjuctf/Contain.py
@@ -1,32 +1,20 @@
+import os
+import docker
 class Contain:
-    def __init__(self,a):
-        self.a =  a
-    def printA(self):
-        print(self.a)
-
-
-
-
-# create table challenge_list(
-#           challenge_id int primary key auto_increment,
-#           challenge_type int(2) not null,
-#           challenge_name varchar(20) not null,
-#           challenge_score int(4) not null,
-#           challenge_hint varchar(30),
-#           docker_flag int(1) not null,
-#           docker_path varchar(30) ,
-#           challenge_flag int(1) not null,
-#           challenge_file varchar(20));
-# type web--0    misc---1 crypto---2  reverse---3
+    def __init__(self):
+        self.path = "./Container/" # jjuctf目录下的
+    def startContain(self,ContainName):
+        pass
+        # 等学一下docker库后再实现功能
+        # cmd  = "cd "+self.path+ContainName+";docker-compose up &"
+        # path = self.path+ContainPath
+        # print(path)
+        # print(cmd)
+        # a = os.popen(cmd)
+        # print(a.readlines())
+    def stopContain(self):
+        pass
 #
+# b = Contain()
+# b.startContain("EasyPython")
 
-# insert into challenge_list
-#   (challenge_name,challenge_type,challenge_score,challenge_hint,docker_flag,challenge_flag)
-#   values("easyPython",0,50,"SSTI漏洞",0,0),("easymisc",1,100,"简单的misc",0,0);
-#
-# id dchallengename type challenge  flag dockerpath
-
-#
-# insert into challenge_list
-#   (challenge_name,challenge_type,challenge_score,challenge_hint,docker_flag,challenge_flag)
-#   values("easycrypto",3,50,"简单逆向",0,0),("easypwn",1,100,"简单的PWN",4,0);
