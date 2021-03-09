4b7b43a64cccd8b3e5ae48b33366f6c9f6b437bc
HSM <905008677@qq.com>
2021-03-09 11:34:39 +0800

    添加初始化IP按钮

--- jjuctf/Crypto.py
+++ jjuctf/Crypto.py
@@ -13,7 +13,7 @@ def add_to_16(text):
 
 # 加密函数
 def encrypt(text):
-    key = '9999999999999999'.encode('utf-8')
+    key = '9999999999999123'.encode('utf-8')
     mode = AES.MODE_CBC
     iv = b'qqqqqqqqqqqqqqqq'
     text = add_to_16(text)
@@ -24,7 +24,7 @@ def encrypt(text):
 
 # 解密后，去掉补足的空格用strip() 去掉
 def decrypt(text):
-    key = '9999999999999999'.encode('utf-8')
+    key = '9999999999999123'.encode('utf-8')
     iv = b'qqqqqqqqqqqqqqqq'
     mode = AES.MODE_CBC
     cryptos = AES.new(key, mode, iv)
